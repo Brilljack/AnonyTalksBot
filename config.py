b5f63e61ad20ce862f10926689e4a22e447b5f4a
Brilljack <91831790+Brilljack@users.noreply.github.com>
2023-10-07 13:28:10 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "6583500052:AAG_UwjOpqPfQUy_tet7S27yEKZLdwZYHG0") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "dzaka_brillian") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://brilljack:Sembarang1@cluster0.we2paev.mongodb.net/?retryWrites=true&w=majority")
