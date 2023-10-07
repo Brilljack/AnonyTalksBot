6a9931b1e2ed795aa631da264a186cc8a5bb9715
Brilljack <91831790+Brilljack@users.noreply.github.com>
2023-10-07 14:00:24 +0700

    merubah pesan lagi

--- Messages.py
+++ Messages.py
@@ -1,4 +1,4 @@
-m_start = "⚡️ SELAMAT DATANG ⚡️\n\n🇮🇩 Halo test\n\n🇳🇿 I hope you can make a friend or a partner\n\n💬 untuk mencari teman obrolan gunakan button New Chat"
+m_start = "⚡️ SELAMAT DATANG ⚡️\n\n🇮🇩 Halo test\n\n🇳🇿 I hope you can make a friend or a partner in this bot\n\n💬 untuk mencari teman obrolan gunakan button New Chat"
 
 m_is_not_free_users = (
     "❗️ Maaf Anda tidak berada dalam obrolan\n\n❗️ Sorry you are not in chat"
